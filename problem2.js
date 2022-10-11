c58f346f66378b86bdd081d70e1eed7ebf8480a7
farhan <farhanjalal99@gmail.com>
2022-10-12 01:37:19 +0530

    web23

---
+++ problem2.js
@@ -0,0 +1,11 @@
+//Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+let name="Farhan";
+let father_name="Jalaluddin";
+let mother_name="Saafia";
+console.log(name+"\n"+father_name+"\n"+mother_name);
